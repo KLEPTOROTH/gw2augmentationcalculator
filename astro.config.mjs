ed6a166be63899a3f1a3a9aa8082096680836687
KLEPTOROTH <screwedbyexisten@gmail.com>
2025-08-15 04:35:48 -0700

    Modify config to load font locally, Add missing submodule dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import icon from 'astro-icon';
+import { fileURLToPath } from 'url'; // <-- Add this import
+import path from 'path'; // <-- Add this import
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,4 +16,15 @@ export default defineConfig({
     }),
     react(),
   ],
+  // Add this entire 'vite' section
+  vite: {
+    resolve: {
+      alias: {
+        '@discretize': path.resolve(
+          path.dirname(fileURLToPath(import.meta.url)),
+          './discretize-ui'
+        ),
+      },
+    },
+  },
 });
