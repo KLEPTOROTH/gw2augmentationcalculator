8f281aacdfb78e928e00c785c48a5bed9b69298a
KLEPTOROTH <screwedbyexisten@gmail.com>
2025-08-15 04:10:29 -0700

    cleaned up config files, minimalized loaded modules

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import icon from 'astro-icon'; // <-- Add this line
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    icon(), // <-- Add this line
+    icon(),
     svelte(),
     tailwind({
       applyBaseStyles: false,
